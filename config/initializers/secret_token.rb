3381ec52c6d4eb1156d3585f0bb1b8dc4213bf48
John Johnson <dampboxdev@gmail.com>
2014-01-14 15:10:39 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BestPractices::Application.config.secret_key_base = '0aea468568ab1a3c84aa78a430f0db25c4b35ca9401f32c0fd80292f93c44619d6a805165b11907191802cad57c507af06e0ed4f5a5245b1e4080395205c321e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BestPractices::Application.config.session_store :cookie_store, key: '_best_practices_session'
